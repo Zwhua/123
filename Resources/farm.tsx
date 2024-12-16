f92cfd0d187b7605dd226d06e0c44bf605520741
Zwhua <161816754+Zwhua@users.noreply.github.com>
2024-12-16 22:03:24 +0800

    Add files via upload

---
+++ Resources/farm.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="farm" tilewidth="16" tileheight="16" tilecount="1975" columns="25">
+ <image source="../../游戏素材/星露谷物语素材/星露谷物语素材/Stardew valley/spring_outdoorsTileSheet.zh-CN..png" width="400" height="1264"/>
+</tileset>
