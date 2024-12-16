79774d7adabdc29a61fce589c3b928c23c344d3b
Zwhua <161816754+Zwhua@users.noreply.github.com>
2024-12-16 19:16:43 +0800

    Add files via upload

---
+++ Resources/farm.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="farm" tilewidth="16" tileheight="16" tilecount="1975" columns="25">
+ <image source="spring_outdoorsTileSheet.zh-CN..png" width="400" height="1264"/>
+</tileset>
